c9943db69e60f2148ce0792587868859b430db43
ibrvimv <ibrvimv@gmail.com>
2024-09-05 21:30:57 +0500

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,52 +3,52 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start px-16 w-ful">
-        <div className=" text-4xl font-bold  h-44 flex items-center">
+      <main className="flex flex-col gap-8  items-center sm:items-start md:px-16 px-8 w-full">
+        <div className=" md:text-4xl text-2xl font-bold h-44 flex items-center w-full">
           StartupScore
         </div>
-        <div className="grid grid-cols-6 w-full">
-          <h1 className="font-inter text-8xl flex flex-col col-span-4 gap-2">
+        <div className="grid max-md:grid-rows-3 gap-5 md:gap-0 w-full md:grid-cols-6">
+          <h1 className="font-inter md:text-8xl text-5xl w-full flex flex-col row-span-1 md:col-span-4 gap-2">
             From Vision <br />
             to Validation with
-            <span className=" bg-primary text-white h-36 flex items-center font-bold w-fit">
+            <span className=" bg-primary text-white md:h-36 h-20 flex items-center font-bold md:w-fit w-full">
               StartupScore
             </span>
           </h1>
-          <div className=' relative col-span-2'>
+          <div className=' relative w-full row-span-2 md:col-span-2'>
             <Image src='/images/banner.png' fill alt='banner' className='object-contain' />
           </div>
         </div>
 
         {/* new  section */}
 
-        <div className='w-full mt-48'>
-          <h2 className='font-bold text-5xl text-center'>Forecast Your Future with <br />
+        <div className='w-full md:mt-48 mt-14'>
+          <h2 className='font-bold md:text-5xl text-3xl text-center'>Forecast Your Future with <br />
             StartupScore</h2>
-          <p className='text-center font-semibold text-2xl mt-10'>The StartupScore predicts a startup&apos;s success rate <br />
+          <p className='text-center font-semibold md:text-2xl text-xl mt-10'>The StartupScore predicts a startup&apos;s success rate <br />
             by using AI to assess its alignment with the target audience and location factors.<br />
             It provides insights into market demand, competition, demographic fit, and potential challenges.</p>
         </div>
 
         {/* new  section */}
 
-        <div className='w-full mt-48 mb-48'>
-          <h2 className='font-bold text-5xl text-center mb-8'>How it works?</h2>
-          <div className='grid grid-cols-3 w-full'>
-            <div className='flex gap-3'>
+        <div className='w-full md:my-48 my-24 '>
+          <h2 className='font-bold md:text-5xl text-3xl text-center mb-8'>How it works?</h2>
+          <div className='grid md:grid-cols-3 w-full grid-rows-3'>
+            <div className='flex gap-3 max-md:grid max-md:grid-cols-2 max-md:items-center'>
               <div className='text-8xl font-inter'>
                 01
               </div>
               <div className='flex-flex-col'>Write your<br /><span className='font-bold'>business idea</span></div>
             </div>
-            <div className='flex gap-3'>
+            <div className='flex gap-3 max-md:grid max-md:grid-cols-2 max-md:items-center'>
               <div className='text-8xl font-inter'>
                 02
               </div>
-              <div className='flex-flex-col'>Describe your<br /><span className='font-bold'>target audience,
+              <div className='flex-flex-col'>Describe your<br /><span className='font-bold'>target audience, <br />
                 location</span></div>
             </div>
-            <div className='flex gap-3'>
+            <div className='flex gap-3 max-md:grid max-md:grid-cols-2 max-md:items-center'>
               <div className='text-8xl font-inter'>
                 03
               </div>
